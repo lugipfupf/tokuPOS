b92cecb3dd7436e3d2a69fe47be722a064395156
Beat Luginbühl <lugi@lugipfupf.ch>
2015-12-28 14:31:16 +0100

    increase default screen size. should actually save and restore current state in properties file, but maybe at a later time

--- src/main/java/com/openbravo/pos/forms/JRootApp.java
+++ src/main/java/com/openbravo/pos/forms/JRootApp.java
@@ -784,7 +784,7 @@ public class JRootApp extends JPanel implements AppView {
         jPanel3 = new javax.swing.JPanel();
 
         setEnabled(false);
-        setPreferredSize(new java.awt.Dimension(1024, 768));
+        setPreferredSize(new java.awt.Dimension(1280, 900));
         setLayout(new java.awt.BorderLayout());
 
         m_jPanelTitle.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, javax.swing.UIManager.getDefaults().getColor("Button.darkShadow")));
@@ -820,7 +820,7 @@ public class JRootApp extends JPanel implements AppView {
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/openbravo/images/unicenta.png"))); // NOI18N
         jLabel1.setText("<html><center>uniCenta oPOS - Touch Friendly Point of Sale<br>" +
-            "Copyright \u00A9 2009-2015 uniCenta <br>" +
+            "Copyright \u00A9 2009-2014 uniCenta <br>" +
             "http://www.unicenta.com<br>" +
             "<br>" +
             "uniCenta oPOS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.<br>" +
@@ -852,7 +852,6 @@ public class JRootApp extends JPanel implements AppView {
 
         m_jLogonName.add(jPanel2, java.awt.BorderLayout.LINE_END);
 
-        jScrollPane1.setBackground(new java.awt.Color(255, 255, 255));
         jScrollPane1.setBorder(null);
         jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         jScrollPane1.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
