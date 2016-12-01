5d033b43b20702d7208125f75409ff6218d53e1f
Beat Luginbühl <lugi@lugipfupf.ch>
2016-12-01 19:18:28 +0100

    pmt37: created file chooser ui

---
+++ src/main/java/com/openbravo/pos/imports/CustomersImportPanel.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.openbravo.pos.imports;
+
+import com.openbravo.data.user.EditorRecord;
+import com.openbravo.data.user.ListProvider;
+import com.openbravo.data.user.SaveProvider;
+import com.openbravo.pos.panels.JPanelTable;
+
+/**
+ *
+ * @author highway
+ */
+public class CustomersImportPanel extends JPanelTable {
+    private JFileChooserPanel jFileChooser;
+    
+    @Override
+    protected void init() {
+        this.jFileChooser = new JFileChooserPanel();
+    }
+
+    @Override
+    public EditorRecord getEditor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ListProvider getListProvider() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public SaveProvider getSaveProvider() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getTitle() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
