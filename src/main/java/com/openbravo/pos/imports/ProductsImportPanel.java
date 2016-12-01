449166c313e2284ec14849bffaafd8cb13de6890
Beat Luginbühl <lugi@lugipfupf.ch>
2016-12-01 14:01:05 +0100

    pmt37: csv import basic setup

---
+++ src/main/java/com/openbravo/pos/imports/ProductsImportPanel.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.openbravo.pos.imports;
+
+import com.openbravo.data.user.EditorRecord;
+import com.openbravo.data.user.ListProvider;
+import com.openbravo.data.user.SaveProvider;
+import com.openbravo.pos.panels.JPanelTable;
+
+/**
+ *
+ * @author highway
+ */
+public class ProductsImportPanel extends JPanelTable {
+
+    @Override
+    protected void init() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public EditorRecord getEditor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ListProvider getListProvider() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public SaveProvider getSaveProvider() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getTitle() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
