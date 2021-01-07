bdd391c655ed462469612bc2b6b64a974e49daf4
Beat Luginbühl <lugi@lugipfupf.ch>
2021-01-07 12:29:02 +0100

    update database and version

--- src/main/java/com/openbravo/pos/forms/AppLocal.java
+++ src/main/java/com/openbravo/pos/forms/AppLocal.java
@@ -40,7 +40,7 @@ public class AppLocal {
     /**
      *
      */
-    public static final String APP_VERSION = "3.92";
+    public static final String APP_VERSION = "3.94";
 
     private static final LocaleResources m_resources;
     
